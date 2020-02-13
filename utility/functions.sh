6c5d5ed61c4e844965fd99bba72450f2c421f880
NaokiTakahashi <s16c1077hq@s.chibakoudai.jp>
2020-02-13 18:53:22 +0900

    Added utility script

---
+++ utility/functions.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+print_config () {
+	echo "Address: $ADDRESS"
+	echo "Channel: $CHANNEL"
+	echo "Device number: $DEVICE_NUMBER"
+	echo "Baud rate: $BAUD_RATE"
+	echo "Charactor size: $CHARACTOR_SIZE"
+}
