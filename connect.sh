4409b50d25367d1502adc5a3f3b0e8e50517624b
NaokiTakahashi <s16c1077hq@s.chibakoudai.jp>
2020-02-24 18:09:38 +0900

    Added script dir

--- connect.sh
+++ connect.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-. utility/functions.sh
-. profile/drogger/dg-pro1rw.sh
+SCRIPT_DIR=`cd $(dirname $0) && pwd`
+. $SCRIPT_DIR/utility/functions.sh
+. $SCRIPT_DIR/profile/drogger/dg-pro1rw.sh
 print_config
 
 sudo rfcomm -rML 0 connect $DEVICE_NUMBER $ADDRESS $CHANNEL
